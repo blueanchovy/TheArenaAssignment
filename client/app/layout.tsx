ea4408cf83475d92b1e6ccf214e66b46f8792e65
Manish Jha <manishjhazyx@gmail.com>
2025-06-08 20:15:02 +0530

    fix: console errors with viem and wagmi versions and pino-pretty webpack config

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 
 export default function RootLayout({
@@ -20,11 +9,9 @@ export default function RootLayout({
 }>) {
   return (
         <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>{children}</Providers>
-        </body>
+          <body>
+            <Providers>{children}</Providers>
+          </body>
         </html>
   );
 }
--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* to resolve https://github.com/rainbow-me/rainbowkit/issues/1368*/
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
