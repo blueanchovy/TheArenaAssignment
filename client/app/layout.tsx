3f826f1f2e86be92a09addc6401fb70475d936b9
Manish Jha <manishjhazyx@gmail.com>
2025-06-11 16:34:59 +0530

    layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import Header from "./(components)/Header";
+import { LeftSidebar } from "./(components)/LeftSidebar";
+import { RightSidebar } from "./(components)/RightSidebar";
 import "./globals.css";
-import { Providers } from "./providers";
+import dynamic from "next/dynamic";
+
+const Providers = dynamic(() => import("./providers").then((mod) => mod.Providers));
 
 
 export default function RootLayout({
@@ -10,7 +17,14 @@ export default function RootLayout({
   return (
         <html lang="en">
           <body>
-            <Providers>{children}</Providers>
+            <Providers>
+              <Header />
+              <div className="flex flex-row h-[calc(100vh-65px)] w-full justify-between">
+                <LeftSidebar />
+                {children}
+                <RightSidebar />
+              </div>
+            </Providers>
           </body>
         </html>
   );
